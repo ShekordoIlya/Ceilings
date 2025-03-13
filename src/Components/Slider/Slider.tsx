4d98b6e4102abe23ffe332defd7ff157d06b0ab3
Ilya Shekordo <27phantom03@list.ru>
2025-03-13 23:06:57 +0300

    feat(slider):works page is done

--- src/Components/Slider/Slider.tsx
+++ src/Components/Slider/Slider.tsx
@@ -8,40 +8,65 @@ import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 
 const SliderCarousel = () => {
   return (
-    <div className="w-[100vw] p-[50px]">
+    <div className="w-[100vw] p-[50px] bg-black mt-16 rounded-[70px]">
       <Swiper
         effect="coverflow"
         grabCursor={true}
         centeredSlides={true}
         slidesPerView={"3"}
         navigation={true}
+        loop={true}
         coverflowEffect={{
           rotate: 50,
-          stretch: 0,
+          stretch: 10,
           depth: 100,
-          modifier: 1,
+          modifier: 3,
           slideShadows: true,
         }}
         pagination={true}
         modules={[EffectCoverflow, Pagination, Navigation]}
-        className="opacity-[99%] w-[100%]"
       >
-        <SwiperSlide className="">
+        <SwiperSlide>
           <img
             src="../../../assets/works/IMG_20210922_170245.jpg"
             alt="first slide"
           />
         </SwiperSlide>
-        <SwiperSlide className="">
+        <SwiperSlide>
           <img
             src="../../../assets/works/IMG_20210928_192711.jpg"
             alt="second slide"
           />
         </SwiperSlide>
-        <SwiperSlide className="">
+        <SwiperSlide>
           <img
             src="../../../assets/works/IMG_20211020_174547.jpg"
             alt="third slide"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="../../../assets/works/IMG_20211228_162117.jpg"
+            alt="fourth slide"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="../../../assets/works/IMG_20231216_031535.jpg"
+            alt="fives slide"
+          />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <img
+            src="../../../assets/works/IMG_20240426_114431.jpg"
+            alt="six slide"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="../../../assets/works/IMG_20211022_152800.jpg"
+            alt="seven slide"
           />
         </SwiperSlide>
       </Swiper>
--- src/Pages/Works.tsx
+++ src/Pages/Works.tsx
@@ -1,7 +1,14 @@
 import SliderCarousel from "../Components/Slider/Slider";
 
 const Works = () => {
-  return <SliderCarousel />;
+  return (
+    <>
+      <p className="text-white mainOpacity text-center lettersBtn font-semibold text-4xl pt-10">
+        Все работы делаем на основании предпочтений и пожеланий клиента
+      </p>
+      <SliderCarousel />
+    </>
+  );
 };
 
 export default Works;
